47801351b746ad8cc32f5a3b6b41d54155a8867e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,30 @@
-output "api_management_custom_domains" {
-  description = "All api_management_custom_domain resources"
-  value       = azurerm_api_management_custom_domain.api_management_custom_domains
-  sensitive   = true
-}
 output "api_management_custom_domains_api_management_id" {
-  description = "List of api_management_id values across all api_management_custom_domains"
-  value       = [for k, v in azurerm_api_management_custom_domain.api_management_custom_domains : v.api_management_id]
+  description = "Map of api_management_id values across all api_management_custom_domains, keyed the same as var.api_management_custom_domains"
+  value       = { for k, v in azurerm_api_management_custom_domain.api_management_custom_domains : k => v.api_management_id }
 }
 output "api_management_custom_domains_developer_portal" {
-  description = "List of developer_portal values across all api_management_custom_domains"
-  value       = [for k, v in azurerm_api_management_custom_domain.api_management_custom_domains : v.developer_portal]
+  description = "Map of developer_portal values across all api_management_custom_domains, keyed the same as var.api_management_custom_domains"
+  value       = { for k, v in azurerm_api_management_custom_domain.api_management_custom_domains : k => v.developer_portal }
   sensitive   = true
 }
 output "api_management_custom_domains_gateway" {
-  description = "List of gateway values across all api_management_custom_domains"
-  value       = [for k, v in azurerm_api_management_custom_domain.api_management_custom_domains : v.gateway]
+  description = "Map of gateway values across all api_management_custom_domains, keyed the same as var.api_management_custom_domains"
+  value       = { for k, v in azurerm_api_management_custom_domain.api_management_custom_domains : k => v.gateway }
   sensitive   = true
 }
 output "api_management_custom_domains_management" {
-  description = "List of management values across all api_management_custom_domains"
-  value       = [for k, v in azurerm_api_management_custom_domain.api_management_custom_domains : v.management]
+  description = "Map of management values across all api_management_custom_domains, keyed the same as var.api_management_custom_domains"
+  value       = { for k, v in azurerm_api_management_custom_domain.api_management_custom_domains : k => v.management }
   sensitive   = true
 }
 output "api_management_custom_domains_portal" {
-  description = "List of portal values across all api_management_custom_domains"
-  value       = [for k, v in azurerm_api_management_custom_domain.api_management_custom_domains : v.portal]
+  description = "Map of portal values across all api_management_custom_domains, keyed the same as var.api_management_custom_domains"
+  value       = { for k, v in azurerm_api_management_custom_domain.api_management_custom_domains : k => v.portal }
   sensitive   = true
 }
 output "api_management_custom_domains_scm" {
-  description = "List of scm values across all api_management_custom_domains"
-  value       = [for k, v in azurerm_api_management_custom_domain.api_management_custom_domains : v.scm]
+  description = "Map of scm values across all api_management_custom_domains, keyed the same as var.api_management_custom_domains"
+  value       = { for k, v in azurerm_api_management_custom_domain.api_management_custom_domains : k => v.scm }
   sensitive   = true
 }
 
